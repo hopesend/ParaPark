42db3af3a2e325389140032a1e7d341d6d18492c
David Ferrandez Molla <dafemoelche@gmail.com>
2016-07-17 09:14:06 +0200

    first commit
    - Primer commit del proyecto

---
+++ Assets/ControlTeclado.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControlTeclado : MonoBehaviour 
+{
+	public ControlTiempo Reloj;
+
+	void Update () 
+	{
+		if (Input.anyKey) 
+		{
+			// (Barra Espaciadora) Comienzo y pausa 
+			if (Input.GetKeyDown (KeyCode.Space)) 
+			{
+				Reloj.pausa = !Reloj.pausa;
+				return;
+			}
+
+			// (Intro) Reinicio de contador
+			if (Input.GetKeyDown (KeyCode.Return)) 
+			{
+				Reloj.tiempo = 0;
+				Reloj.MostrarTiempo ();
+				return;
+			}
+
+			// (F5) aumenta de 5 en 5 el tiempo
+			if (Input.GetKeyDown (KeyCode.F5)) 
+			{
+				Reloj.tiempo += 300;
+				Reloj.MostrarTiempo ();
+				return;
+			}
+
+			// (F10) aumenta de 10 en 10 el tiempo
+			if (Input.GetKeyDown (KeyCode.F10)) 
+			{
+				Reloj.tiempo += 600;
+				Reloj.MostrarTiempo ();
+				return;
+			}
+		}
+	}
+}
---
+++ Assets/ControlTiempo.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ControlTiempo : MonoBehaviour 
+{
+	public Text segundos;
+	public Text minutos;
+	public Text horas;
+	public bool pausa;
+	public float tiempo;
+
+	private float s;
+	private float m;
+	private float h;
+
+	void Start()
+	{
+		pausa = true;
+	}
+
+	void Update () 
+	{
+		if (!pausa) 
+		{
+			tiempo += Time.deltaTime;
+			MostrarTiempo ();
+		}
+	}
+
+	public void MostrarTiempo()
+	{
+		minutos.text = Mathf.Floor (tiempo / 60) >= 60 ? ((tiempo/60)-60).ToString("00") : Mathf.Floor (tiempo / 60).ToString ("00");
+		segundos.text = Mathf.Floor (tiempo % 60) >= 60 ? "00" : Mathf.Floor (tiempo % 60).ToString ("00");
+		horas.text = Mathf.Floor (tiempo / 3600) >= 60 ? ((tiempo/3600)-60).ToString("00") : Mathf.Floor (tiempo / 3600).ToString ("00");
+	}
+}
