b9aea78de9ee75a332b685e2eb41580974d7fa40
David Ferrandez Molla <dafemoelche@gmail.com>
2016-09-09 06:36:18 +0200

    ...
    ...

---
+++ Assets/CM_MicInput.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CM_MicInput : MonoBehaviour 
+{
+	public AudioSource audioSource;
+	public bool debug;
+	public bool muteMicrophone;
+	public List<string> availableMicrophones;
+
+	private AudioClip microphone;
+
+	void Awake()
+	{
+		availableMicrophones = new List<string> ();
+		debug = false;
+		muteMicrophone = false;
+		audioSource = this.gameObject.GetComponent<AudioSource> ();
+	}
+
+	void Start () 
+	{
+		availableMicrophones.Add ("Default");
+		availableMicrophones.AddRange (Microphone.devices);
+
+	}
+	
+	void Update () 
+	{
+		audioSource.clip = Microphone.Start("Built-in Microphone", true, 10, 44100);
+	}
+}
